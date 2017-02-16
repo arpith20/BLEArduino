e4f87d6de1f68a3b4184edbd72df2ce37d403198
Arpith K <arpith.k@csa.iisc.ernet.in>
2017-02-16 11:13:20 +0530

    Added Arduino sketch

---
+++ arduino/arduino.ino
@@ -0,0 +1,43 @@
+#include <SoftwareSerial.h>
+#define LED_PIN 13
+#define RELAY1 7
+#define RELAY2 8
+
+SoftwareSerial mySerial(2, 4); // RX, TX  
+// Connect HM10 or HC08   Arduino Uno
+//     Pin 1/TXD          Pin 2
+//     Pin 2/RXD          Pin 4
+
+void setup() {
+  Serial.begin(9600);
+
+  // If the baudrate of the HM-10 module has been updated,
+  // you may need to change 9600 by another value
+  // Once you have found the correct baudrate,
+  // you can update it using AT+BAUDx command 
+  // e.g. AT+BAUD0 for 9600 bauds
+  mySerial.begin(9600);
+}
+
+void loop() {
+  int c;
+
+  if (mySerial.available()) {
+    c = mySerial.read();
+    Serial.println(c);
+    Serial.println("Got input:");
+    if (c != 48) {
+      // Non-zero input means "turn on LED".
+      digitalWrite(RELAY2, 0);
+      Serial.println("  on");
+      //digitalWrite(LED_PIN, HIGH);
+    } else {
+      // Input value zero means "turn off LED".
+      digitalWrite(RELAY2, 1);
+      Serial.println("  off");
+      //digitalWrite(LED_PIN, LOW);
+    }
+    delay(1000);
+  }
+}
+
